b681e4c8ae7a6e27d333b948e198143bbabe3b77
bbx <bbx>
2025-02-26 00:26:16 +0800

    0

--- diamond-grid.js
+++ diamond-grid.js
@@ -18,7 +18,7 @@ customElements.define(tagName, class extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({mode: 'open'}).append(
-            E('link', {rel: 'stylesheet', href: `https://aeoq.github.io/${tagName}/${tagName}.css`}),
+            E('link', {rel: 'stylesheet', href: `./${tagName}.css`}),
             E('slot', {onslotchange: (ev) => [this.shape(ev), this.rearrange(ev)]})
         );
         new ResizeObserver(([entry]) => {
@@ -30,33 +30,34 @@ customElements.define(tagName, class extends HTMLElement {
     }
     #oldWidth;
     connectedCallback() {
-        isNaN(new E(this).get('--side')) && new A({'--side': '20em'}).apply(this);
-        isNaN(new E(this).get('--gap')) && new A({'--gap': '.5em'}).apply(this);
+        let E_this = E(this);
+        isNaN(E_this.get('--side')) && E_this.set({'--side': '20em'});
+        isNaN(E_this.get('--gap')) && E_this.set({'--gap': '.5em'});
         setTimeout(() => this.hidden = false, 1000);
     }
     shape (ev) {
         ev.target.assignedElements().forEach(el => el.Q('shape') || el.prepend(...[0,0].map(_ => E('span', {classList: 'shape'}))));
     }
     rearrange(ev) {
-        let children = ev?.target.assignedElements() ?? [...this.children];
-        new A(this.#reset).apply(children);
-        children = children.filter(child => !child.hidden);
-        if (!children.length) return;
+        let items = ev?.target.assignedElements() ?? [...this.children];
+        items.forEach(item => E(item).set(this.#reset));
+        items = items.filter(item => !item.hidden);
+        if (!items.length) return;
 
         let W = this.getBoundingClientRect().width,
             g = parseFloat(getComputedStyle(this).gap),
-            w = children[0].getBoundingClientRect().width;
+            w = items[0].getBoundingClientRect().width;
         let more = Math.floor((W + g) / (w + g)),
             less = Math.floor((2 * W - w + g) / 2 / (w + g));
 
         if (more === less)
-            return children.forEach((child, i) => new A(this.#position[Math.ceil((i + 1) / more) % 2 === 0 ? 'right' : 'left']).apply(child));
+            return items.forEach((item, i) => E(item).set(this.#position[Math.ceil((i + 1) / more) % 2 === 0 ? 'right' : 'left']));
 
         let n = 1, i;
-        while (children[i = (more + less) * n - less]) {
+        while (items[i = (more + less) * n - less]) {
             let j = 0;
-            while (j <= more - 1 && children[i + j]) {
-                new A(this.#position[j < more - 1 ? 'center' : 'next']).apply(children[i + j]);
+            while (j <= more - 1 && items[i + j]) {
+                E(items[i + j]).set(this.#position[j < more - 1 ? 'center' : 'next']);
                 j++;
             }
             n++;
