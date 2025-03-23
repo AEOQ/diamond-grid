512f7d6bfa220d4c8eeb40e5923422b2005ee001
AEOQ <200552650+AEOQ@users.noreply.github.com>
2025-03-24 00:30:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -44,10 +44,10 @@ customElements.define(tagName, class extends HTMLElement {
         items = items.filter(item => !item.hidden);
         if (!items.length) return;
 
-        this.hidden = true;
+        this.style.opacity = 0;
         Promise.all(items.filter(item => item.tagName == 'IMG').map(img => new Promise(res => img.complete ? res() : img.onload = res)))
         .then(() => {
-            this.hidden = false;
+            this.style.opacity = 1;
             let W = this.getBoundingClientRect().width,
                 g = parseFloat(getComputedStyle(this).gap),
                 w = items[0].getBoundingClientRect().width;
